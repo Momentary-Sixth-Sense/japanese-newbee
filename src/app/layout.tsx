43957e29dd100598a7d771e89837b3620d9a7ac5
russoooo <churl99@dreamus.io>
2025-07-08 16:25:01 +0900

    fix: meta-tag 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "히라가나 가타카나 외우기",
+  title: "일본어 문자 퀴즈 - 히라가나 & 가타카나",
   description: "히라가나 가타카나 외울떄까지 반복해서 퀴즈를 풀어보세요",
 };
 
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
